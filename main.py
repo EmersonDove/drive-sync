fa0b1b3fd1f2f311c49c7514e0ce792b25b4477a
Emerson Dove <52636744+EmersonDove@users.noreply.github.com>
2024-02-16 02:04:10 -0500

    "upgrade" to parallel downloading

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
-import os
+import concurrent.futures
 import io
-import json
+import os
+from pathlib import Path
+
+from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
-from pathlib import Path
+from threading import RLock
+
+WRITE_LOCK = RLock()
 
 # The SCOPES defines the level of access you are requesting from Google Drive.
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 
 
+class JobExecutor:
+    def __init__(self, max_workers):
+        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=max_workers)
+        self.futures = []
+
+    def submit_job(self, func, *args):
+        # Wait until there is a free worker
+        if len(self.futures) >= self.executor._max_workers:
+            # Wait for at least one future to complete before submitting a new job
+            print(f"Waiting for at least one future to complete before submitting a new job. {len(self.futures)} jobs in queue.")
+            done, _ = concurrent.futures.wait(self.futures, return_when=concurrent.futures.FIRST_COMPLETED)
+            # Clean up completed futures
+            self.futures = [f for f in self.futures if f not in done]
+
+        # Submit new job
+        future = self.executor.submit(func, *args)
+        self.futures.append(future)
+        return future
+
+    def wait_completion(self):
+        # Wait for all jobs to complete
+        concurrent.futures.wait(self.futures)
+
+
 def main():
+    max_workers = 5
+    job_executor = JobExecutor(max_workers)
+
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -34,10 +65,14 @@ def main():
     service = build('drive', 'v3', credentials=creds)
 
     print("Listing files and folders:")
-    initial_local_path = "/Volumes/Google Drive Backup/edove@vt.edu-01:27:24"  # Or any path you prefer
+    # initial_local_path = "/Volumes/Google Drive Backup/edove@vt.edu-01:27:24"  # Or any path you prefer
+    initial_local_path = "./test"
     # Make this directory
     os.makedirs(initial_local_path, exist_ok=True)
-    list_all_files(service, 'root', '', initial_local_path)
+    list_all_files(service, 'root', '', initial_local_path, job_executor)
+
+    print("waiting for final jobs")
+    job_executor.wait_completion()
 
 
 def write_failed_folder(folder_id: str, folder_name: str, error: str):
@@ -56,7 +91,7 @@ def write_failed_downloads(file_id: str, file_name: str, error: str):
         f.write(f"{file_id},{file_name},{error}\n")
 
 
-def list_all_files(service, folder_id, indent, local_path):
+def list_all_files(service, folder_id, indent, local_path, job_executor: JobExecutor):
     page_token = None
     while True:
         # Fetch files and folders within the current folderID, accounting for pagination
@@ -82,7 +117,7 @@ def list_all_files(service, folder_id, indent, local_path):
 
                     print(f"{indent}[Folder] {item['name']}")
                     try:
-                        list_all_files(service, item['id'], indent + "  ", new_local_path)
+                        list_all_files(service, item['id'], indent + "  ", new_local_path, job_executor)
                     except Exception as e:
                         print(f"{indent}Failed to list folder {item['name']} with error: {e}")
                         write_failed_folder(item['id'], local_path, str(e))
@@ -90,12 +125,8 @@ def list_all_files(service, folder_id, indent, local_path):
                     # Download the file
                     file_path = os.path.join(local_path, item['name'] + '.pdf' if item['mimeType'].startswith('application/vnd.google-apps.') else item['name'])
                     print(f"{indent}[Downloading file] {item['name']}")
-                    try:
-                        download_file(service, item['id'], file_path, item['mimeType'])
-                    except Exception as e:
-                        print(f"{indent}Failed to download file {item['name']} with error: {e}")
-                        write_failed_downloads(item['id'], file_path, str(e))
 
+                    job_executor.submit_job(download_file, service, item['id'], file_path, item['mimeType'])
 
         if page_token is None:
             print(f"{indent}Completed folder {folder_id}.")
@@ -115,34 +146,39 @@ def sanitize_path(path: str) -> Path:
 
 
 def download_file(service, file_id, file_path, mime_type):
-    sanitized_path = sanitize_path(file_path)
-    temp_path = Path(str(sanitized_path) + ".temp")
+    global WRITE_LOCK
+    try:
+        sanitized_path = sanitize_path(file_path)
+        temp_path = Path(str(sanitized_path) + ".temp")
 
-    # Check if file already exists
-    if os.path.exists(sanitized_path):
-        print(f"File {sanitized_path} already exists. Skipping download.")
-        return
+        # Check if file already exists
+        if os.path.exists(sanitized_path):
+            print(f"File {sanitized_path} already exists. Skipping download.")
+            return
 
-    # Determine the appropriate method to download the file based on its MIME type
-    if mime_type.startswith('application/vnd.google-apps.'):
-        # It's a Google Workspace document, so we'll export it as a PDF.
-        request = service.files().export_media(fileId=file_id,
-                                               mimeType='application/pdf')
-        sanitized_path = sanitized_path.with_suffix('.pdf')  # Ensure the file has a .pdf extension
-    else:
-        # It's a different kind of file, download without conversion.
-        request = service.files().get_media(fileId=file_id)
+        # Determine the appropriate method to download the file based on its MIME type
+        if mime_type.startswith('application/vnd.google-apps.'):
+            # It's a Google Workspace document, so we'll export it as a PDF.
+            request = service.files().export_media(fileId=file_id,
+                                                   mimeType='application/pdf')
+            sanitized_path = sanitized_path.with_suffix('.pdf')  # Ensure the file has a .pdf extension
+        else:
+            # It's a different kind of file, download without conversion.
+            request = service.files().get_media(fileId=file_id)
 
-    fh = io.FileIO(temp_path, 'wb')
-    downloader = MediaIoBaseDownload(fh, request)
-    done = False
-    while not done:
-        status, done = downloader.next_chunk()
-        print(f"Download {int(status.progress() * 100)}%.")
+        fh = io.FileIO(temp_path, 'wb')
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while not done:
+            status, done = downloader.next_chunk()
+            print(f"Download {int(status.progress() * 100)}%.")
 
-    # Rename the temp file to the final file name
-    os.rename(temp_path, sanitized_path)
-    print(f"Downloaded '{sanitized_path}' successfully.")
+        # Rename the temp file to the final file name
+        os.rename(temp_path, sanitized_path)
+        print(f"Downloaded '{sanitized_path}' successfully.")
+    except Exception as e:
+        print(f"Failed to download file {file_path} with error: {e}")
+        write_failed_downloads(file_id, file_path, str(e))
 
 
 if __name__ == '__main__':
